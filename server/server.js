0f39d4b0cb130cee183946345415af64a7e968fe
Bonevy BEBY <bonevybeby@gmail.com>
2024-08-28 00:36:04 +0200

    added: api exported and tested

--- server/server.js
+++ server/server.js
@@ -3,10 +3,13 @@ const bodyParser=require('body-parser')
 
 const PORT=3000
 
+const api=require('./routes/api')
+
 const app=express()
 
 app.use(bodyParser.json())
 
+app.use('/api',api)
 
 app.get('/',function(req,res){
     res.send('Hello from server');
